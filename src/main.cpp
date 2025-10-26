7ef830507093722e459d64d62af2a4240235fd42
Jesse Deuel <jesse@deuels.net>
2025-10-25 22:45:48 -0500

    Added pindefs

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <Arduino.h>
+#include "pindefs.h"
+#include <Wire.h>
 
 void setup() {
-    // put your setup code here, to run once:
+    pinMode(PWM_A_1, OUTPUT);
+    pinMode(PWM_A_2, OUTPUT);
+    pinMode(PWM_A_3, OUTPUT);
+    pinMode(PWM_B_1, OUTPUT);
+    pinMode(PWM_B_2, OUTPUT);
+    pinMode(PWM_B_3, OUTPUT);
+    pinMode(PWM_C_1, OUTPUT);
+    pinMode(PWM_C_2, OUTPUT);
+    pinMode(PWM_C_3, OUTPUT);
+
+    pinMode(NSLEEP_1, OUTPUT);
+    pinMode(NSLEEP_2, OUTPUT);
+    pinMode(NSLEEP_3, OUTPUT);
+    
+    pinMode(NFAULT_1, INPUT);
+    pinMode(NFAULT_2, INPUT);
+    pinMode(NFAULT_3, INPUT);
+
+    pinMode(DRVOFF, OUTPUT);
+
+    Wire.begin();
+
 }
 
 void loop() {
---
+++ src/pindefs.h
@@ -0,0 +1,28 @@
+#define PWM_A_1 1
+#define PWM_A_2 4
+#define PWM_A_3 7
+
+#define PWM_B_1 2
+#define PWM_B_2 5
+#define PWM_B_3 8
+
+#define PWM_C_1 3
+#define PWM_C_2 6
+#define PWM_C_3 9
+
+#define NSLEEP_1 28
+#define NSLEEP_2 29
+#define NSLEEP_3 26
+
+#define NFAULT_1 35
+#define NFAULT_2 30
+#define NFAULT_3 31
+
+#define DRVOFF 14
+
+#define SDA 18
+#define SCL 19
+
+#define SO_1 41
+#define SO_2 40
+#define SO_3 39
